aff55625c48d482ab220ecefc9e0844ebca1a514
Tyler Dunston <tylerdunston@macbook-air.mynetworksettings.com>
2024-06-04 21:04:42 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const str1 = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+
+var combination1 = 2 + 2 + 6;
+
+//The result should be 40
+var combination2 = 80 / 2;
+
+//The result should be 39.
+var combination3 = 42 - 2 - 1;
+const strcomplete = '${str1} ${combination1} ${combination2} ${combination3}!';
+
+console.log(strcomplete);
+// Here is the secret combination:
